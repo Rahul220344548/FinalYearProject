18db710a4698c00aeeba303347133c9ca5bb6fb3
Rahul Vasantlal <220344548@aston.ac.uk>
2025-01-30 22:05:36 +0000

    my first commit

---
+++ app/src/main/java/com/example/gym_application/model/User.kt
@@ -0,0 +1,6 @@
+package com.example.gym_application.model
+
+data class User(
+    var Email: String,
+    var Password: String,
+)
---
+++ app/src/main/java/com/example/gym_application/model/UserPersonalDetails.kt
@@ -0,0 +1,3 @@
+package com.example.gym_application.model
+
+data class UserPersonalDetails(val name: String)
