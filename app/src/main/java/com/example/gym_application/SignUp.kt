861eda0d8610f9642b06759e32db0230194f4393
Rahul Vasantlal <220344548@aston.ac.uk>
2025-02-03 15:58:26 +0000

    added logic to navigate to login screen if sign in link is clicked

--- app/src/main/java/com/example/gym_application/SignUp.kt
+++ app/src/main/java/com/example/gym_application/SignUp.kt
@@ -1,7 +1,16 @@
 package com.example.gym_application
 
+import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.TextPaint
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.view.View
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -23,8 +32,32 @@ class SignUp : AppCompatActivity() {
 
         val backArrow = findViewById<ImageView>(R.id.back_icon)
         backArrow.setOnClickListener {
-            Toast.makeText(this, "Your message here", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "You clicked back button", Toast.LENGTH_SHORT).show()
         }
+
+        val textView = findViewById<TextView>(R.id.textViewSignIn)
+        val text = "Already have an account? Sign in"
+
+        val spannableString = SpannableString(text)
+        val clickableSpan = object : ClickableSpan() {
+            override fun onClick(widget: View) {
+                // Navigate to LoginActivity (Replace with your activity)
+                val intent = Intent(applicationContext, MainActivity::class.java)
+                startActivity(intent)
+            }
+
+            override fun updateDrawState(ds: TextPaint) {
+                super.updateDrawState(ds)
+                ds.isUnderlineText = true  // Underline the text
+                ds.color = Color.BLUE  // Change text color to blue
+            }
+        }
+        val start = text.indexOf("Sign in")
+        val end = start + "Sign in".length
+        spannableString.setSpan(clickableSpan, start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        textView.text = spannableString
+        textView.movementMethod = LinkMovementMethod.getInstance()
 
     }
 }
