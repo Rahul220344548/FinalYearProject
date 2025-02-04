ea9575ac1e30c194becca44960720a792bfc0201
Rahul Vasantlal <220344548@aston.ac.uk>
2025-02-04 01:18:43 +0000

    remove register code in main activity kt file

--- app/src/main/java/com/example/gym_application/MainActivity.kt
+++ app/src/main/java/com/example/gym_application/MainActivity.kt
@@ -2,20 +2,14 @@ package com.example.gym_application
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.view.View
-import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.gym_application.model.User
-import com.example.gym_application.model.UserPersonalDetails
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 
 
@@ -23,8 +17,6 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var emailEditText: EditText
     private lateinit var passwordEditText: EditText
-    private lateinit var registerButton: Button
-    private lateinit var loginButton: Button
 
     private lateinit var auth: FirebaseAuth
     private lateinit var database: FirebaseDatabase
@@ -45,74 +37,11 @@ class MainActivity : AppCompatActivity() {
 
         emailEditText = findViewById(R.id.editTextEmail)
         passwordEditText = findViewById(R.id.editTextPassword)
-        registerButton = findViewById(R.id.buttonRegister)
-        loginButton = findViewById(R.id.buttonLogin)
-
-        registerButton.setOnClickListener { registerUser() }
-        loginButton.setOnClickListener { loginUser() }
-    }
-    //function to register users
-     fun registerUser() {
-        val email = emailEditText.text.toString().trim()
-        val password = passwordEditText.text.toString().trim()
-
-        if (email.isEmpty() || password.isEmpty()) {
-            Toast.makeText(this, "Please fill in all fields", Toast.LENGTH_SHORT).show()
-            return
-        }
-
-        auth.createUserWithEmailAndPassword(email, password)
-            .addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-                    val userId = auth.currentUser?.uid
-                    if (userId != null) {
-                        saveUserData(userId, email, "test111")
-                    }
-                } else {
-                    Toast.makeText(this, "Registration failed: ${task.exception?.message}", Toast.LENGTH_SHORT).show()
-                }
-            }
     }
 
-    /**@param email: this is string
-     * @param name: this is name string
-     * @return saves data to the database table
-     * */
-    private fun saveUserData(userId: String, email: String, name: String) {
-        val user = User(userId, email)
-
-        val userPersonalDetails = UserPersonalDetails(name)
-
-        addUserData(userId, user);
-        addUserDetailData(userId, userPersonalDetails);
-
-    }
-
-    private fun addUserData(userId: String, user: User): String {
-        database.reference.child("users").child(userId).setValue(user)
-            .addOnSuccessListener {
-                Toast.makeText(this, "User registered successfully!", Toast.LENGTH_SHORT).show()
-            }
-            .addOnFailureListener {
-                Toast.makeText(this, "Failed to save user data: ${it.message}", Toast.LENGTH_SHORT).show()
-            }
-        val test: DatabaseReference = database.getReference("users").child(userId)
-        Toast.makeText(this,test.toString(),Toast.LENGTH_SHORT).show()
-        return "test"
-    }
-
-    private fun addUserDetailData(userId: String, userPersonalDetails: UserPersonalDetails){
-        database.reference.child("userPersonalDetails").child(userId).setValue(userPersonalDetails)
-            .addOnSuccessListener {
-                Toast.makeText(this, "User registered successfully!", Toast.LENGTH_SHORT).show()
-            }
-            .addOnFailureListener {
-                Toast.makeText(this, "Failed to save user data: ${it.message}", Toast.LENGTH_SHORT).show()
-            }
-    }
 
     // Login user
-    private fun loginUser() {
+    fun loginUser(view: View) {
         val email = emailEditText.text.toString().trim()
         val password = passwordEditText.text.toString().trim()
 
@@ -134,13 +63,7 @@ class MainActivity : AppCompatActivity() {
             }
     }
 
-
+    fun goToSignUpPage(view: View) {
+        startActivity(Intent(this, SignUp::class.java))
+    }
 }
-
-//    fun registerUser(view: View) {
-//        Toast.makeText(this@MainActivity, emailEditText.text.toString(), Toast.LENGTH_SHORT).show()
-//    }
-//
-//    fun loginUser(view: View) {
-//        Toast.makeText(this, "Login successful", Toast.LENGTH_SHORT).show()
-//    }
