fa1285dcc2e1898978cfc0dbef7e0d709a15bcc0
Rahul Vasantlal <220344548@aston.ac.uk>
2025-02-15 20:29:48 +0000

    added navigation fragments

--- app/src/main/java/com/example/gym_application/admin_view/AdminDashboardActivity.kt
+++ app/src/main/java/com/example/gym_application/admin_view/AdminDashboardActivity.kt
@@ -8,6 +8,8 @@ import androidx.core.view.WindowInsetsCompat
 import com.example.gym_application.R
 
 class AdminDashboardActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
---
+++ app/src/main/java/com/example/gym_application/admin_view/navigation_fragments/AdminHomeFragment.kt
@@ -0,0 +1,22 @@
+package com.example.gym_application.admin_view.navigation_fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.gym_application.R
+
+
+class AdminHomeFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_admin_home, container, false)
+    }
+
+}
