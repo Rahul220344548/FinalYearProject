0df03607d3d6d21174f588ed8fea1db203189e9a
Rahul Vasantlal <220344548@aston.ac.uk>
2025-02-18 15:22:31 +0000

    add design in create class page for basic class details

--- app/src/main/java/com/example/gym_application/admin_view/navigation_fragments/AdminClassesFragment.kt
+++ app/src/main/java/com/example/gym_application/admin_view/navigation_fragments/AdminClassesFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import com.example.gym_application.R
 
 
@@ -16,7 +18,27 @@ class AdminClassesFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_admin_classes, container, false)
+//        return inflater.inflate(R.layout.fragment_admin_classes, container, false)
+
+        val rootView = inflater.inflate(R.layout.fragment_admin_classes, container, false)
+
+        val colors = resources.getStringArray(R.array.colors)
+
+        val arrayAdapter = ArrayAdapter(requireContext(), android.R.layout.simple_dropdown_item_1line, colors)
+
+        val autoCompleteTextView = rootView.findViewById<AutoCompleteTextView>(R.id.auto_complete_txt)
+
+        autoCompleteTextView.setAdapter(arrayAdapter)
+
+        autoCompleteTextView.setOnItemClickListener { parent, view, position, id ->
+            val selectedColor = parent.getItemAtPosition(position) as String
+            // Do something with the selected color
+        }
+
+        return rootView
+
+
+
     }
 
 }
