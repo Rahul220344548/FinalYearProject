bfeb1527139b30001c4b7ca61ca310be9ed9f450
Rahul Vasantlal <220344548@aston.ac.uk>
2025-02-18 16:38:49 +0000

    refactored code to move create class information in a new activity

---
+++ app/src/main/java/com/example/gym_application/admin_view/CreateAClass.kt
@@ -0,0 +1,42 @@
+package com.example.gym_application.admin_view
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.gym_application.R
+
+class CreateAClass : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_create_aclass)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    private fun dropdown() {
+
+//        val rootView = inflater.inflate(R.layout.fragment_admin_classes, container, false)
+//
+//        val colors = resources.getStringArray(R.array.colors)
+//
+//        val arrayAdapter = ArrayAdapter(requireContext(), android.R.layout.simple_dropdown_item_1line, colors)
+//
+//        val autoCompleteTextView = rootView.findViewById<AutoCompleteTextView>(R.id.auto_complete_txt)
+//
+//        autoCompleteTextView.setAdapter(arrayAdapter)
+//
+//        autoCompleteTextView.setOnItemClickListener { parent, view, position, id ->
+//            val selectedColor = parent.getItemAtPosition(position) as String
+//            // Do something with the selected color
+//        }
+//
+//        return rootView
+    }
+
+}
--- app/src/main/java/com/example/gym_application/admin_view/navigation_fragments/AdminClassesFragment.kt
+++ app/src/main/java/com/example/gym_application/admin_view/navigation_fragments/AdminClassesFragment.kt
@@ -1,5 +1,6 @@
 package com.example.gym_application.admin_view.navigation_fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,38 +8,33 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
+import android.widget.Button
+import com.example.gym_application.HomeActivity
 import com.example.gym_application.R
+import com.example.gym_application.SignUp
+import com.example.gym_application.admin_view.CreateAClass
 
 
 class AdminClassesFragment : Fragment() {
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_admin_classes, container, false)
 //        return inflater.inflate(R.layout.fragment_admin_classes, container, false)
 
-        val rootView = inflater.inflate(R.layout.fragment_admin_classes, container, false)
+        val addClassBtn: Button = view.findViewById(R.id.btnAddClass)
 
-        val colors = resources.getStringArray(R.array.colors)
-
-        val arrayAdapter = ArrayAdapter(requireContext(), android.R.layout.simple_dropdown_item_1line, colors)
-
-        val autoCompleteTextView = rootView.findViewById<AutoCompleteTextView>(R.id.auto_complete_txt)
-
-        autoCompleteTextView.setAdapter(arrayAdapter)
-
-        autoCompleteTextView.setOnItemClickListener { parent, view, position, id ->
-            val selectedColor = parent.getItemAtPosition(position) as String
-            // Do something with the selected color
+        addClassBtn.setOnClickListener {
+            goToCreateAClassPage()
         }
-
-        return rootView
-
-
+        return view
 
     }
 
+    fun goToCreateAClassPage() {
+        val intent = Intent(requireActivity(), CreateAClass::class.java)
+        startActivity(intent)
+    }
 }
